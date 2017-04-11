6d014814b44bae44b9e05a1cac7b5e5c00f57680
nasir <nasirosman1@hotmail.com>
2017-04-10 18:17:36 -0600

    login verification and logging out for all three types of end users

--- app.py
+++ app.py
@@ -47,18 +47,83 @@ def Authenticate():
 		return "Logged in succesfully"
 
 
+@app.route('/admin')
+def admin():
+	if(session['admin']):
+		return render_template('index.html')
+	else:
+		return main()
+
+
+
 @app.route('/admin_login', methods = ['GET', 'POST'])
 def admin_login():
-	return -1		
+	adminId = request.args.get('adminId')
+	password = request.args.get('Password')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from Admins where adminId ='" + adminId + "' and Password = '" + password +"'")
+	if data is None:
+		return main()
+	else:
+		session['admin'] = True
+		return admin()
+	
+@app.route('admin_logoff')
+def admin_logoff():
+	session['admin'] = False
+	return main() 
+
+
+@app.route('coach')
+def coach():
+	if(session['coach']):
+		return render_template('coach.html')
+	else:
+		return main()
+
 
 
 @app.route('/coach_login', methods = ['GET', 'POST'])
 def coach_login():
-	return -1
+	coachId = request.args.get('coachId')
+	password = request.args.get('Password')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from Coaches where coachId ='" + adminId + "' and Password = '" + password +"'")
+	if data is None:
+		return main()
+	else:
+		session['coach'] = True
+		return coach()
+
+
+
+@app.route('/athlete_logout')
+def coach_logout():
+	session['athlete'] = False
+	return main() 
+
+@app.route('/coach_logout')
+	session['coach'] = False
+	return main() 
+
+@app.route('/athlete')
+def athlete():
+	if(session['athlete']):
+		return render_template('athlete.html')
+	else:
+		return main()
 
 @app.route('/athlete_login', methods = ['GET', 'POST'])
 def athlete_login():
- 	return ""
+ 	athleteId = request.args.get('athleteId')
+	password = request.args.get('Password')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from Athletes where athleteId ='" + adminId + "' and Password = '" + password +"'")
+	if data is None:
+		return main()
+	else:
+		session['athlete'] = True
+		return athlete()
  	"""
  	if 'ath_user' in session:
  		return redirect(url_for('ath_dashboard'))
